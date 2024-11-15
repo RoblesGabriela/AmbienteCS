8e5d6afebef1a91a646138430cd87b1d7be3b4cb
Aleao22 <alearias538@gmail.com>
2024-11-14 22:02:38 -0600

    edit

--- image/ejemplo/test.bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-
-if($conn){
-    echo "Connected successfully to the database.";
-} else {
-    echo "Failed to connect to the database. Error: " . mysqli_connect_error();
-}
